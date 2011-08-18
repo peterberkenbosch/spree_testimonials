247f5e8743c33d7c4c6e8764227c90388fe6216b
Pierre <pierre.basile@gmail.com>
2011-08-18 20:12:51 +0200

    switch to deface

--- lib/spree_testimonials_hooks.rb
+++ lib/spree_testimonials_hooks.rb
@@ -1,5 +1,8 @@
 class SpreeTestimonialsHooks < Spree::ThemeSupport::HookListener
-  insert_after :admin_tabs do
-    %(<%=  tab(:testimonials)  %>)
-  end
+  Deface::Override.new(:virtual_path => "layouts/admin",
+                       :name => "converted_admin_tabs_923933875",
+                       :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
+                       :text => "<%=  tab(:testimonials)  %>",
+                       :disabled => false)
+
 end
