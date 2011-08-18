80805581ff9f26a21a589bda205ceb22d749f1b4
Pierre <pierre.basile@gmail.com>
2011-08-18 20:55:00 +0200

    add paginate to admin views

--- app/controllers/admin/testimonials_controller.rb
+++ app/controllers/admin/testimonials_controller.rb
@@ -1,6 +1,10 @@
 class Admin::TestimonialsController < Admin::BaseController
   resource_controller
 
+  def index
+    @testimonials = Testimonial.order("created_at DESC").paginate(:per_page => 30, :page => params[:page])
+  end
+
   create.response do |format|
     format.html { redirect_to collection_url }
   end
